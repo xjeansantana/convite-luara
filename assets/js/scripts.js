31d651ecc312cf01484356b842a997f1eeefeb96
Jean de Santana <51293417+xjeansantana@users.noreply.github.com>
2024-04-23 16:58:12 -0300

    ok

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,17 @@
 //const convidado = document.getElementById9('convidado');
 
-const convidado = prompt('Olá......Digite seu nome: ');
-document.getElementById('convidado').innerHTML = convidado;
+/*const convidado = prompt('Olá......Digite seu nome: ');
+document.getElementById('convidado').innerHTML = convidado;*/
 
 function confirmarPresenca() {
     window.open("https://api.whatsapp.com/send?phone=5575991601163&text=Oi,%20estou%20confirmando%20a%20minha%20presen%C3%A7a.", "_blank");
 }
+
+function sugestao() {
+    window.location.href = "sugestoes.html";
+}
+
+function localizacao() {
+    window.open("https://maps.app.goo.gl/8PqRp9ZuqQXtu4dt9", "_blank");
+}
+
