19b80d568abc74957d3eb54c0f023d055a30c82c
Jean de Santana <51293417+xjeansantana@users.noreply.github.com>
2024-04-24 11:05:49 -0300

    ok

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 //const convidado = document.getElementById9('convidado');
 
 const convidado = prompt('Olá...Digite seu nome: ');
-document.getElementById('convidado').innerHTML = convidado;*/
+document.getElementById('convidado').innerHTML = convidado;
 
 function confirmarPresenca() {
     window.open("https://api.whatsapp.com/send?phone=5575991601163&text=Oi,%20estou%20confirmando%20a%20minha%20presen%C3%A7a.", "_blank");
